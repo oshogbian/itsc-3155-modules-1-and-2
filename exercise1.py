20e834fe212ed743f1c4aa3e06acf33e1efa2e5e
oshogbian <142279566+oshogbian@users.noreply.github.com>
2024-01-24 09:35:35 -0500

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,13 @@
+def add_numbers(a: int, b: int) -> int:
+    """
+    Adds two numbers and returns the result.
+
+    Args:
+        a: The first number to add.
+        b: The second number to add.
+
+    Returns:
+        The sum of a and b.
+    """
+    sum = a + b
+    return sum
